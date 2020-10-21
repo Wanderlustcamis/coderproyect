6136c323924f936839c8d2c2d0d7554e0b736063
wanderlustcamis <germanophile@gmail.com>
2020-10-21 17:41:44 -0300

    ;

--- Java/js.js
+++ Java/js.js
@@ -2,4 +2,4 @@ var textito = alert ("Hi");
 var nombre = prompt ("What's your name?");
 var leer = ", why don't you visit my IG?";
 var dialogo = alert ('Welcome ' + nombre + leer);
-console.log (nombre)
+console.log (nombre);
